3d252c6fd0ea590035d59feaf27e7f9a38150fee
bhoward06 <89262363+bhoward06@users.noreply.github.com>
2021-08-23 10:39:17 -0400

    Add files via upload

---
+++ Aug-18.Rd.R
@@ -0,0 +1,21 @@
+# Author: Howard, Date: 08/18/2021, Purpose: Calculate mean, median, mode
+
+# Dummy data
+
+variable1 <- c(10, 9, 8, 7, 6)
+
+# calculate the mean/average of variable1
+
+mean <- mean(variable1)
+#8
+
+# calculate median value
+
+median <- median (variable1)
+# 8
+
+#calculate mode value
+variable2 <- c(10, 10, 9, 8, 7)
+
+# install the external packages in R
+install.packages("modeest")
