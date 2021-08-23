61bfd975e372f00a85999a1bfcbe48c0f5297dd4
bhoward06 <89262363+bhoward06@users.noreply.github.com>
2021-08-23 11:09:26 -0400

    Add files via upload

---
+++ Aug-16.Rd.R
@@ -0,0 +1,13 @@
+# Author: Howard, Date: Aug 16, 2021, Purpose: Calculate stdev, Median values.
+
+# Calculate standard deviation
+
+# A variable to hold 7 numbers.
+variable1 <- c(10, 7, 4, 5, 2, 3, 1)
+
+sd(variable1)
+# 3.101459
+
+# Calculate median.
+median(variable1)
+# 4
