d4c729e0b9b97d1944290496b93f65b2eba81649
bhoward06 <89262363+bhoward06@users.noreply.github.com>
2021-10-08 15:52:12 -0400

    Add files via upload

---
+++ oct-8.Rd.R
@@ -0,0 +1,8 @@
+# Author: Howard, Date: Oct 6 2021, Purpose: To calculate the probability distribution values 
+
+# Calculate Continuous Uniform distribution probability
+runif(10, min=1, max=3)
+
+# Find the 95th percentile of the Chi-Squared distribution with 7 degrees of freedom
+
+qchisq(.95, df=7)
